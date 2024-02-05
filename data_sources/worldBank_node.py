ea30e85a4e422b3a5744749bd7a486c3227c558f
Mikoláš Fromm <mikolas.fromm@gmail.com>
2024-02-05 22:03:31 +0100

    Finished workbank node

--- data_sources/worldBank_node.py
+++ data_sources/worldBank_node.py
@@ -1,4 +1,6 @@
 from instances.node import *
+from nltk.stem.snowball import SnowballStemmer
+from copy import deepcopy
 from typing import List
 from consts import *
 
@@ -29,4 +31,24 @@ class WorldBankNode(OperationNode):
             return []
         
         if (not self.expanded):
-            ...
+            if (mode == WORKER_MODE.KEYWORD_GEN_AND_MATCH): ## filtering when generating tree based on keywords
+                stemmer = SnowballStemmer("english", ignore_stopwords=True)
+                keywords = [stemmer.stem(x.strip().casefold()) for x in params['query'].split()]
+                self.children = [child for child in self.children if any(stemmer.stem(word.strip().casefold()) in keywords for word in child.textual_name.split())]
+
+            if (mode == WORKER_MODE.MATCH_AND_FILTER):
+                stemmer = SnowballStemmer("english", ignore_stopwords=True)
+                keywords = [stemmer.stem(x.strip().casefold()) for x in params['query'].split()]
+                self.children = [child for child in self.children if any(stemmer.stem(word.strip().casefold()) in keywords for word in child.textual_name.split())]
+
+        if (remaining_depth > 1):
+            for child in self.children:
+                new_params = deepcopy(params)
+                new_params['look_ahead_depth'] = remaining_depth - 1
+                child.expand(mode, params, cache)
+        
+        self.expanded = True
+        return self.children
+    
+    def get_final_str_desc(self) -> str:
+        return f"{self.textual_name}"
