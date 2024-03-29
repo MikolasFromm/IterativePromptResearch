860a937fc4b8deeab12fa9a27b3095bb73cf2dbe
Mikoláš Fromm <mikolas.fromm@gmail.com>
2024-03-29 22:57:46 +0100

    Added statistical tests for graphs

--- research/consts.py
+++ research/consts.py
@@ -1,8 +1,8 @@
 from types import SimpleNamespace
 
 NUM_OF_KEYWORDS = 40
-MODEL = "gpt-4-0125-preview"
-# MODEL = "gpt-3.5-turbo"
+# MODEL = "gpt-4-0125-preview"
+MODEL = "gpt-3.5-turbo"
 
 WORKER_TYPE = SimpleNamespace()
 WORKER_TYPE.WEBPAGE = 0
